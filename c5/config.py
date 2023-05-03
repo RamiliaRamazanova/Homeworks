455d18b5ae27eb5b3ed1e1ac6b6008c56b768451
ramazanova <ramramilg@gmail.com>
2023-05-04 00:58:47 +0300

    TGBot

---
+++ c5/config.py
@@ -0,0 +1,11 @@
+TOKEN = "5531488334:AAEyiap-C7_dLCV1IGAdZJ2m3P1lyZHLuH0"
+
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'юань': 'CNY',
+    'крона': 'CZK',
+    'тенге': 'KZT',
+    'биткоин' : 'BTC'
+}
